3b46fb064dd16d9b44a34c1518e8fc21c144520a
Harol Zhang <harol.zhang@outlook.com>
2019-09-07 19:00:02 -0400

    Updated Headers

--- src/main/java/com/example/demo/Filter/CORSFilter.java
+++ src/main/java/com/example/demo/Filter/CORSFilter.java
@@ -13,7 +13,7 @@ public class CORSFilter implements Filter
     {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
